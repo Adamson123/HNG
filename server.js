8655725ce1967e4404197d218e427bf981c24732
Ajibade Adam <dapoajibade66@gmail.com>
2024-07-01 15:13:59 +0100

    server er fixed

--- server.js
+++ server.js
@@ -12,26 +12,29 @@ app.get("/", (req, res) => {
   res.send(" <h1>Welcome to ....<a href='/api/hello?visitor_name=Adam'>Test</a></h1>");
 });
 
-
+const apiKey = process.env.WEATHER_API_KEY
 
 app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
 
   try {
-    const apiKey = process.env.WEATHER_API_KEY;
+   
     let userIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     
-    userIp = userIp.split(',')[0].trim();
+    userIp = userIp.split(',')[0];
+    console.log(userIp);
 
     
     const geoRes = await axios.get(`http://ip-api.com/json/${userIp}`);
     
     console.log(geoRes);
-    const { city: location, lat, lon } = geoRes.data;
+    const { location, lat, lon } = geoRes.data.city;
 
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
     const weatherResponse = await axios.get(weatherUrl);
     const temperature = weatherResponse.data.main.temp;
+
+
 
     const responseObj = {
       client_ip: userIp,
@@ -41,8 +44,8 @@ app.get("/api/hello", async (req, res) => {
 
     res.status(200).json(responseObj);
   } catch (error) {
-    console.error(`Error:`, error.message);
-    res.status(500).send("Error: Something went wrong");
+    //console.error(`Error:`, error.message);
+    res.status(500).send("Something went wrong");
   }
 });
 
