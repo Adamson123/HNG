a40f9861e51c0c9e992bc4fdb48025a194611ede
Ajibade Adam <dapoajibade66@gmail.com>
2024-07-01 15:20:04 +0100

    rs

--- server.js
+++ server.js
@@ -1,52 +1,41 @@
-require("dotenv/config")
 const express = require("express");
 const axios = require("axios");
 const app = express();
-
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.set("trust proxy", true);
 
 app.get("/", (req, res) => {
-  res.send(" <h1>Welcome to ....<a href='/api/hello?visitor_name=Adam'>Test</a></h1>");
+  res.send("Welcome to .... <a href='/api/hello?visitor_name=Adam'>Test</a>");
 });
-
-
-const apiKey = "41031635df0050e559be040d9c04356a"
 
 app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
 
   try {
-   
+    const apiKey = "41031635df0050e559be040d9c04356a";
     let userIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    
-    userIp = userIp.split(',')[0];
-    console.log(userIp);
+    userIp = userIp.split(',')[0].trim();
 
     
     const geoRes = await axios.get(`http://ip-api.com/json/${userIp}`);
-    
-    console.log(geoRes);
-    const { location, lat, lon } = geoRes.data.city;
+    const { city: location, lat, lon } = geoRes.data;
 
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
     const weatherResponse = await axios.get(weatherUrl);
     const temperature = weatherResponse.data.main.temp;
 
-
-
     const responseObj = {
       client_ip: userIp,
       location,
-      greeting: `Hello, ${visitor_name ? visitor_name : "Unknown🙄"}!, the temperature is ${temperature} degrees Celsius in ${location}`,
+      greeting: `Hello, ${visitor_name ? visitor_name : "Bossman"}!, the temperature is ${temperature} degrees Celsius in ${location}`,
     };
 
     res.status(200).json(responseObj);
   } catch (error) {
-    //console.error(`Error:`, error.message);
-    res.status(500).send("Something went wrong");
+    console.error(`Error:`, error.message);
+    res.status(500).send("Error: Something went wrong");
   }
 });
 
