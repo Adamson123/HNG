5717ec50c33892ffdfd5a2cd2b693bd24cc764b4
Ajibade Adam <111712386+Adamson123@users.noreply.github.com>
2024-07-01 12:39:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const axios = require("axios");
-const geoip = require("geoip-lite");
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -8,9 +7,7 @@ app.use(express.json());
 app.set("trust proxy", true);
 
 app.get("/", (req, res) => {
-  res.send(
-    "Welcome to .... <a href='/api/hello?visitor_name=Adam'>Test</a>"
-  );
+  res.send("Welcome to .... <a href='/api/hello?visitor_name=Adam'>Test</a>");
 });
 
 app.get("/api/hello", async (req, res) => {
@@ -18,33 +15,30 @@ app.get("/api/hello", async (req, res) => {
 
   try {
     const apiKey = "41031635df0050e559be040d9c04356a";
-    const getPulicIp = await axios.get("https://api.ipify.org?format=json");
-    let publicIp = req.headers["x-forwarded-for"] || req
-.connection.remoteAddress;
-    publicIp = publicIp.split(',')[0].trim()
-   console.log(publicIp)
-    const geoipLoc = geoip.lookup(publicIp);
+    let publicIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    publicIp = publicIp.split(',')[0].trim();
 
-    const location = geoipLoc.city;
+    
+    const geoRes = await axios.get(`http://ip-api.com/json/${publicIp}`);
+    const { city: location, lat, lon } = geoRes.data;
 
-    const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${geoipLoc.ll[0]}&lon=${geoipLoc.ll[1]}&units=metric&exclude=hourly,daily&appid=${apiKey}`;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    const weatherResponse = await axios.get(weatherUrl);
+    const temperature = weatherResponse.data.main.temp;
 
-   // console.log(geoipLoc.ll);
-    const getTemperature = await axios.get(openWeatherUrl);
-
-    const resObj = {
+    const responseObj = {
       client_ip: publicIp,
       location,
-      greeting: `Hello, ${visitor_name}!, the temperature is ${getTemperature.data.main.temp} degrees Celsius in ${location}`,
+      greeting: `Hello, ${visitor_name}!, the temperature is ${temperature} degrees Celsius in ${location}`,
     };
 
-    res.status(200).json(resObj);
+    res.status(200).json(responseObj);
   } catch (error) {
-    console.error(`Error`, error.message);
-    res.status(500).send("Error Something went wrong");
+    console.error(`Error:`, error.message);
+    res.status(500).send("Error: Something went wrong");
   }
 });
 
 app.listen(5002, () => {
-  console.log("Server is onnn ✔✔😁");
+  console.log("Server is running on port 5002 ✔✔😁");
 });
